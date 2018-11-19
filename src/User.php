87b6ccaa7915e4291e3c01336209ea409c36ad13
Bob Wezelman <bob.wezelman@hotmail.com>
2018-11-19 16:29:14 +0100

    Started on users

--- src/User.php
+++ src/User.php
@@ -1,6 +1,32 @@
 <?php
-namespace BitbucketWrapper;
-class User
-{
 
+namespace BitbucketWrapper;
+
+use GuzzleHttp\Client;
+
+class User extends Base
+{
+    protected $url = 'https://api.bitbucket.org/2.0/user';
+
+    /**
+     * User constructor.
+     * @param Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get()
+    {
+        return $this->request($this->url);
+    }
+
+    public function update()
+    {
+
+    }
 }
